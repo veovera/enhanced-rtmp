7851401ffea07aaf9aed92f80c2adcb80d0f1415
slozben <slavik@lozben.com>
2025-04-18 12:00:48 -0700

    chore: replace license header with SPDX form and fix warning

--- wip/web-player/src/mux-lib/player/player-engine-dedicated-thread.ts
+++ wip/web-player/src/mux-lib/player/player-engine-dedicated-thread.ts
@@ -1,23 +1,13 @@
 /*
- * Copyright (C) 2023 zheng qian. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  *
+ * Copyright (C) 2023 zheng qian
  * @author zheng qian <xqq@xqq.im>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This file has been modified. See Git history for details.
  */
 
 import EventEmitter from 'eventemitter3';
-import * as work from 'webworkify-webpack';
+import work from 'webworkify-webpack';
 import type PlayerEngine from './player-engine';
 import Log from '../utils/logger';
 import LoggingControl from '../utils/logging-control.js';
