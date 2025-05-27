69b851c470e6ac5d110adc63602cddf384223fc5
ozloz <YOUR_NO_REPLY_EMAIL>
2025-05-26 20:08:41 -0700

    refactor: create Remuxer abastract class for handling media remuxing with abstract methods

---
+++ wip/web-player/src/mux-lib/remux/remuxer.ts
@@ -0,0 +1,58 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (C) 2025 Veovera Software Organization.
+ * @author Slavik Lozben
+ */
+
+import { Callback } from '../utils/common.js';
+import { AudioMetadata, AudioTrackInfo, FLVDemuxer, VideoMetadata, VideoTrackInfo } from '../demux/flv-demuxer.js';
+import { TrackType } from '../core/media-segment-info.js';
+import { ConfigOptions } from '../config.js';
+
+export type RemuxerType = 'mp4' | 'webm';
+
+export interface InitSegment {
+  type: TrackType;
+  data: ArrayBuffer;
+  codec: string;
+  container: string;
+  mediaDuration: number;
+}
+
+export abstract class Remuxer {
+  abstract destroy(): void;
+  abstract bindDataSource(producer: FLVDemuxer): this;
+  abstract insertDiscontinuity(): void;
+  abstract seek(originalDts: number): void;
+  abstract flushStashedSamples(): void;
+
+  abstract get timestampBase(): number | undefined;
+
+  // Callback properties
+  abstract get onInitSegment(): Callback;
+  abstract set onInitSegment(callback: Callback);
+  abstract get onMediaSegment(): Callback;
+  abstract set onMediaSegment(callback: Callback);
+
+  protected abstract _onTrackData(audioTrack: AudioTrackInfo, videoTrack: VideoTrackInfo): void;
+  protected abstract _onTrackMetadata(metadata: AudioMetadata | VideoMetadata): void;
+
+  protected _config: ConfigOptions;
+  protected _isLive: boolean;
+  protected _isAudioMetadataSet = false;
+  protected _isVideoMetadataSet = false;
+
+  constructor(config: ConfigOptions) {
+    this._config = config;
+    this._isLive = config.isLive;
+  }
+
+  get isAudioMetadataDispatched(): boolean {
+    return this._isAudioMetadataSet;
+  }
+
+  get isVideoMetadataDispatched(): boolean {
+    return this._isVideoMetadataSet;
+  }
+} 
