5a4ff8fbb82c058144bf56cae6997d254d11f662
ozloz <YOUR_NO_REPLY_EMAIL>
2025-05-27 11:56:06 -0700

    chore: add unofficial progress notes and todo list for the project

---
+++ wip/web-player/todo.ts
@@ -0,0 +1,31 @@
+/*
+
+These are unofficial notes, to help keep track of the progress. It's meant to be simple and concise and is not meant to be a comprehensive official roadmap.
+
+Now
+* av1 webm
+  - select correct mime type for mse
+  - generate init segment
+  - generate media segment
+
+To do 
+* av1 webm
+* vp9 webm
+* vp8 webm
+* opus webm
+* flac webm
+* play audio only flv
+* address !!@
+* remove mpegts
+  - remove mpegts-demuxer
+  - remove mpegts-muxer
+  - remove mpegts-parser
+  - remove mpegts-remuxer
+* fix compilation errors
+* consolidate tsconfig.json
+* remove destroy() from all classes
+* build a test harness
+
+Done
+
+*/
