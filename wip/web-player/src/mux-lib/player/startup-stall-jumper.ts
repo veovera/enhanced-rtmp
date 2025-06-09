22edfc497344a73a2e45e8006a784558ec79de75
ozloz <YOUR_NO_REPLY_EMAIL>
2025-06-09 13:37:56 -0700

    chore: update copyright information and clean up variable initializations

--- wip/web-player/src/mux-lib/player/startup-stall-jumper.ts
+++ wip/web-player/src/mux-lib/player/startup-stall-jumper.ts
@@ -1,19 +1,13 @@
 /*
- * Copyright (C) 2023 zheng qian. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  *
+ * Copyright (C) 2023 zheng qian.
  * @author zheng qian <xqq@xqq.im>
+ * 
+ * Modified by Slavik Lozben.
+ * Additional changes Copyright (C) 2025 Veovera Software Organization.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See Git history for full details.
  */
 
 import Log from '../utils/logger';
@@ -22,8 +16,8 @@ class StartupStallJumper {
 
     private readonly TAG: string = 'StartupStallJumper';
 
-    private _media_element: HTMLMediaElement = null;
-    private _on_direct_seek: (target: number) => void = null;
+    private _media_element: HTMLMediaElement;
+    private _on_direct_seek: (target: number) => void;
     private _canplay_received: boolean = false;
 
     private e: any = null;
@@ -47,8 +41,6 @@ class StartupStallJumper {
         this._media_element.removeEventListener('canplay', this.e.onMediaCanPlay);
         this._media_element.removeEventListener('stalled', this.e.onMediaStalled);
         this._media_element.removeEventListener('progress', this.e.onMediaProgress);
-        this._media_element = null;
-        this._on_direct_seek = null;
     }
 
     private _onMediaCanPlay(e: Event): void {
