194172da8cc31032176eaa4c41466a79f0d7cae4
ozloz <YOUR_NO_REPLY_EMAIL>
2025-08-15 10:51:06 -0700

    fix: add MPEGAudioObjectType enum and initialize properties in MP3Data class

--- wip/web-player/src/mux-lib/demux/mp3.ts
+++ wip/web-player/src/mux-lib/demux/mp3.ts
@@ -1,7 +1,21 @@
-export class MP3Data {
-    object_type: number;
-    sample_rate: number;
-    channel_count: number;
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Modified by Slavik Lozben.
+ * Additional changes Copyright (C) 2025 Veovera Software Organization.
+ *
+ * See Git history for full details.
+ */
 
-    data: Uint8Array;
+export enum MPEGAudioObjectType {
+    Layer1 = 32,   // MPEG-4 Audio Object Type for MPEG Layer 1
+    Layer2 = 33,   // MPEG-4 Audio Object Type for MPEG Layer 2
+    Layer3 = 34    // MPEG-4 Audio Object Type for MPEG Layer 3 (MP3)
+}
+
+export class MP3Data {
+    object_type: number = MPEGAudioObjectType.Layer3;   // MPEG audio version
+    sample_rate: number = 44100;                        // Sampling frequency
+    channel_count: number = 2;                          // Number of audio channels
+    data: Uint8Array = new Uint8Array();                // Audio data
 }
