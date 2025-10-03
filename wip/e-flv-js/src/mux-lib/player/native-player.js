55e98caed30492136b3dbe792e6d84396edc34b9
ozloz <YOUR_NO_REPLY_EMAIL>
2025-10-03 13:25:27 -0700

    docs: standardize license header in native-player.js

--- wip/e-flv-js/src/mux-lib/player/native-player.js
+++ wip/e-flv-js/src/mux-lib/player/native-player.js
@@ -1,10 +1,13 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright (C) 2016 Bilibili
+ * Copyright (C) 2016 Bilibili.
  * @author zheng qian <xqq@xqq.im>
  * 
- * This file has been modified. See Git history for details.
+ * Modified by Slavik Lozben.
+ * Additional changes Copyright (C) 2025 Veovera Software Organization.
+ *
+ * See Git history for full details.
  */
 
 import EventEmitter from 'eventemitter3';
